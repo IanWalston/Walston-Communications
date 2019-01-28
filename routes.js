d236482275fe8a9a3152572af2af0bb172e757d4
Ian Walston <ikwalston@comcast.net>
2019-01-28 17:26:40 -0500

    replaced carosel with list

--- routes.js
+++ routes.js
@@ -8,7 +8,9 @@ $about.onclick = () => {
   $display.innerHTML = "";
   $display.innerHTML = `
   <div class="row">
-    <div class="col-md-5"><img class="img-fluid rotated" src="img/about.jpg" /></div>
+  <div class="col-md-1"></div>
+    <div class="col-md-3"><br><br><img class="img-fluid rotated" src="img/about.jpg"/><br><br><br></div>
+    <div class="col-md-1"></div>
     <div class="col-md-7">
       <div class="row"><h2>remember how you hated writing book reports?</h2></div>
       <div class="row m-2 p-2">
@@ -16,8 +18,11 @@ $about.onclick = () => {
           We love English in all its forms, written or spoken. That's why, since
           1988, we've been turning our affection for words into award-winning,
           multifaceted marketing and public relations campaigns for some of the
-          world's best-known organizations, from Apple to the United Nations. We
-          seamless move from the traditional -- writing for brochures, reports,
+          world's best-known organizations, from Apple to the United Nations.
+          </p>
+          <p> 
+          
+          We seamless move from the traditional -- writing for brochures, reports,
           newsletters and journals, print and broadcast ads -- with the digital --
           mobile, social media, e-mail, videos, blogs, websites -- to produce
           communications of concision, precision, and impact. We take our writing
@@ -33,45 +38,50 @@ $work.onclick = () => {
   event.preventDefault();
   $display.innerHTML = "";
   $display.innerHTML = `<div class='row'>
-    <h3>Look at this portfolio</h3>
+    <h3>the proof is in the pudding.</h3>
 </div>
 <hr>
 <div class="row">
-<div
-  id="carouselExampleControls"
-  class="carousel slide"
-  data-ride="carousel"
->
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img class="d-block w-100" src="./img/firstslide.svg" alt="First slide" />
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="./img/secondslide.svg" alt="Second slide" />
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="./img/thirdslide.svg" alt="Third slide" />
-    </div>
-  </div>
-  <a
-    class="carousel-control-prev"
-    href="#carouselExampleControls"
-    role="button"
-    data-slide="prev"
-  >
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a
-    class="carousel-control-next"
-    href="#carouselExampleControls"
-    role="button"
-    data-slide="next"
-  >
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
+<img class="img-fluid" src="./img/portfolio/3.jpg" />
 </div>
+<div class="row">
+<img class="img-fluid" src="./img/portfolio/4.jpg" />
+</div>
+<div class="row">
+<img class="img-fluid" src="./img/portfolio/5.jpg" />
+</div>
+<div class="row">
+<img class="img-fluid" src="./img/portfolio/6.jpg" />
+</div>
+<div class="row">
+<img class="img-fluid" src="./img/portfolio/7.jpg" />
+</div>
+<div class="row">
+<img class="img-fluid" src="./img/portfolio/8.jpg" />
+</div>
+<div class="row">
+<img class="img-fluid" src="./img/portfolio/9.jpg" />
+</div>
+<div class="row">
+<img class="img-fluid" src="./img/portfolio/10.jpg"/>
+</div>
+<div class="row">
+<img class="img-fluid" src="./img/portfolio/11.jpg"/>
+</div>
+<div class="row">
+<img class="img-fluid" src="./img/portfolio/12.jpg"/>
+</div>
+<div class="row">
+<img class="img-fluid" src="./img/portfolio/13.jpg"/>
+</div>
+<div class="row">
+<img class="img-fluid" src="./img/portfolio/14.jpg"/>
+</div>
+<div class="row">
+<img class="img-fluid" src="./img/portfolio/15.jpg"/>
+</div>
+<div class="row">
+<img class="img-fluid" src="./img/portfolio/16.jpg"/>
 </div>`;
 };
 
@@ -81,13 +91,13 @@ $contact.onclick = () => {
   $display.innerHTML = `
     <div class='row'>
         <h3>
-            Contact me
+            lets chat about your needs.
         </h3>
     </div> 
     <hr>
     <hr>
     <div class='row'>
-        <h4>Mark Walston</h4>
+        <h3>Mark Walston</h3>
         <hr>
     </div>
     <div class='row'>
